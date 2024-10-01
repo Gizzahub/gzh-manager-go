0467b0c66d929956f007e43e2f8701638224d89f
CEE <archmagece@users.noreply.github.com>
2024-10-02 01:37:31 +0900

    fix(ci): go version

--- cmd/setclone_github.go
+++ cmd/setclone_github.go
@@ -2,7 +2,7 @@ package cmd
 
 import (
 	"fmt"
-	githubpkg "github.com/gizzahub/gzh-manager-go/pkg/github"
+	"github.com/gizzahub/gzh-manager-go/pkg/github"
 	"github.com/spf13/cobra"
 )
 
@@ -36,7 +36,7 @@ func (o *setcloneGithubOptions) run(_ *cobra.Command, args []string) error {
 		return fmt.Errorf("both targetPath and orgName must be specified")
 	}
 
-	err := githubpkg.RefreshAll(o.targetPath, o.orgName)
+	err := github.RefreshAll(o.targetPath, o.orgName)
 	if err != nil {
 		return err
 	}
